9461e62ba8e867d497f3e3c450dca224a0465e50
gianbdev <giancarlo.silva550@gmail.com>
2024-09-17 20:58:27 -0500

    update | v1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+// import { AuthGuard } from './shared/gaurd/auth.guard';
 
-const routes: Routes = [];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+const routes: Routes = [
+  /*
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  {
+    path: '',
+    loadChildren: () => import('./core/core.module').then((m) => m.CoreModule),
+  },*/
+  {
+    path: '',
+    loadChildren: () =>
+      import('./authentication/authentication.module').then(
+        (m) => m.AuthenticationModule,
+      ),
+  },
+  /*
+  {
+    path: 'error',
+    loadChildren: () =>
+      import('./error/error.module').then((m) => m.ErrorModule),
+  },
+  { path: '**', redirectTo: 'error/error404', pathMatch: 'full' },
+   */
+];
+@NgModule({ imports: [RouterModule.forRoot(routes)], exports: [RouterModule] })
+export class AppRoutingModule {}
