241f2bbe50c3d2641449bccca92813374370a513
gianbdev <giancarlo.silva550@gmail.com>
2024-10-03 17:07:04 -0500

    clean authentication component

--- src/app/authentication/login/login.module.ts
+++ src/app/authentication/login/login.module.ts
@@ -3,15 +3,9 @@ import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 import { SharedModule } from 'src/app/shared/shared.module';
-//import { ReactiveFormsModule } from '@angular/forms'; // Importa ReactiveFormsModule
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [
-    CommonModule,
-    LoginRoutingModule,
-    SharedModule,
-    //ReactiveFormsModule,
-  ],
+  imports: [CommonModule, LoginRoutingModule, SharedModule],
 })
 export class LoginModule {}
